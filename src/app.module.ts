e8ff5283ad2433698db9f6e4cc7dd77e22d1c5cd
Vyctor <dev.vyctor@gmail.com>
2024-08-25 22:52:34 -0300

    build: adicionando servicos ao docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvironmentService } from './config/environment.service';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [EnvironmentService],
+      useFactory: (environmentService: EnvironmentService) => ({
+        type: environmentService.DB_TYPE as any,
+        host: environmentService.DB_URL,
+        port: environmentService.DB_PORT,
+        database: environmentService.DB_NAME,
+        username: environmentService.DB_USER,
+        password: environmentService.DB_PASS,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [EnvironmentService],
+      useFactory: (environmentService: EnvironmentService) => ({
+        store: redisStore.redisStore as unknown as CacheStore,
+        host: environmentService.REDIS_HOST,
+        port: environmentService.REDIS_PORT,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
