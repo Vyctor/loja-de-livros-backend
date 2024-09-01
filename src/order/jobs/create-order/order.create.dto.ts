4049058c3de38449996d7412b132517e1fdd198c
Vyctor <dev.vyctor@gmail.com>
2024-09-01 12:26:58 -0300

    feat: criando validacao do OrderCreateDto

--- src/order/jobs/create-order/order.create.dto.ts
+++ src/order/jobs/create-order/order.create.dto.ts
@@ -1,29 +1,87 @@
+import {
+  IsCreditCard,
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsPhoneNumber,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+import {
+  CardBrand,
+  OrderPaymentType,
+} from '../../../order/entities/order-payment.entity';
+import { Type } from 'class-transformer';
+
+export class OrderPaymentDto {
+  @IsEnum(OrderPaymentType)
+  type: string;
+  @IsEnum(CardBrand)
+  card_brand: string;
+  @IsCreditCard()
+  card_number: string;
+  @IsString()
+  card_holder: string;
+  @IsString()
+  card_expiration: string;
+  @IsString()
+  card_cvv: string;
+}
+export class OrderItemDto {
+  @IsNumber()
+  @Min(1)
+  quantity: number;
+  @IsNumber()
+  @Min(1)
+  book_id: number;
+  @IsNumber()
+  @Min(1)
+  price: number;
+}
+
+export class OrderClientDto {
+  @IsEmail()
+  email: string;
+  @IsString()
+  name: string;
+  @IsString()
+  surname: string;
+  @IsString()
+  document: string;
+  @IsNumber()
+  country_id: number;
+  @IsNumber()
+  state_id: number;
+  @IsString()
+  street_name: string;
+  @IsString()
+  street_number: string;
+  @IsString()
+  complement: string;
+  @IsString()
+  zip_code: string;
+  @IsPhoneNumber()
+  phone: string;
+}
+
 export class OrderCreateDto {
+  @IsNumber({
+    allowNaN: false,
+    maxDecimalPlaces: 10,
+  })
+  @Min(1)
   total: number;
-  client: {
-    email: string;
-    name: string;
-    surname: string;
-    document: string;
-    country_id: number;
-    state_id: number;
-    street_name: string;
-    street_number: string;
-    complement: string;
-    zip_code: string;
-    phone: string;
-  };
-  items: Array<{
-    quantity: number;
-    book_id: number;
-    price: number;
-  }>;
-  payment: {
-    type: string;
-    card_brand: string;
-    card_number: string;
-    card_holder: string;
-    card_expiration: string;
-    card_cvv: string;
-  };
+
+  @ValidateNested()
+  @Type(() => OrderClientDto)
+  client: OrderClientDto;
+
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: Array<OrderItemDto>;
+
+  @ValidateNested()
+  @Type(() => OrderPaymentDto)
+  payment: OrderPaymentDto;
 }
